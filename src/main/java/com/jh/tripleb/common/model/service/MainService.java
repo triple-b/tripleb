ee7b17389246ca885c68cbc09a7736903cd30f98
KIMJI <64935736+triple-b@users.noreply.github.com>
2020-05-16 17:11:43 +0900

    주희 커밋

--- src/main/java/com/jh/tripleb/common/model/service/MainService.java
+++ src/main/java/com/jh/tripleb/common/model/service/MainService.java
@@ -1,5 +1,16 @@
 package com.jh.tripleb.common.model.service;
 
 public interface MainService {
+	
+	int currentClass(int tno);
+	
+	int currentStudent(int tno);
+	
+	int newStudent(int tno);
+	
+	int expStudent(int tno);
 
+	int thisMonth();
+	
+	int thisMonthGoal();
 }
--- src/main/java/com/jh/tripleb/common/model/service/MainServiceImpl.java
+++ src/main/java/com/jh/tripleb/common/model/service/MainServiceImpl.java
@@ -1,5 +1,48 @@
 package com.jh.tripleb.common.model.service;
 
-public class MainServiceImpl {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.jh.tripleb.common.model.dao.MainDao;
+
+@Service("umService")
+public class MainServiceImpl implements MainService {
+	
+	@Autowired
+	SqlSessionTemplate sqlSession;
+	@Autowired
+	MainDao umDao;
+
+	@Override
+	public int currentClass(int tno) {
+		return umDao.currentClass(sqlSession, tno);
+	}
+
+	@Override
+	public int currentStudent(int tno) {
+		return umDao.currentStudent(sqlSession, tno);
+	}
+
+	@Override
+	public int newStudent(int tno) {
+		return umDao.newStudent(sqlSession, tno);
+	}
+
+	@Override
+	public int expStudent(int tno) {
+		return umDao.expStudent(sqlSession, tno);
+	}
+
+	@Override
+	public int thisMonth() {
+		return umDao.thisMonth(sqlSession);
+	}
+
+	@Override
+	public int thisMonthGoal() {
+		return umDao.thisMonthGoal(sqlSession);
+	}
+	
 
 }
